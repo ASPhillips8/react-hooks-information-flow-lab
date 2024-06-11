26f10297e225551f828f5983ede0ab5adf2567ed
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-11 18:37:29 -0400

    Create Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
+
+function Header({isDarkMode, onDarkModeClick}) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+
+  );
+}
+
+export default Header;
